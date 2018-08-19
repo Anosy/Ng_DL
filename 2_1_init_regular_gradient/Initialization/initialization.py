9fddf52bc9dcbd171f1b802240a63ca54b87e416
Anosy <543695676@qq.com>
2018-08-19 22:11:04 +0800

    init and regular

--- 2_1_init_regular_gradient/Initialization/initialization.py
+++ 2_1_init_regular_gradient/Initialization/initialization.py
@@ -9,4 +9,84 @@ plt.rcParams['figure.figsize'] = (7.0, 4.0)
 plt.rcParams['image.interpolation'] = 'nearest'
 plt.rcParams['image.cmap'] = 'gray'
 
-train_X, train_Y, test_X, test_Y = load_dataset()
+def initialize_parameters_zeros(layers_dims):
+
+    parameters = {}
+    L = len(layers_dims)
+
+    for l in range(1, L):
+        parameters['W' + str(l)] = np.zeros((layers_dims[l],layers_dims[l-1]))
+        parameters['b' + str(l)] = np.zeros((layers_dims[l],1))
+    return parameters
+
+
+def initialize_parameters_random(layers_dims):
+
+    np.random.seed(3)
+    parameters = {}
+    L = len(layers_dims)
+
+    for l in range(1, L):
+        parameters['W' + str(l)] = np.random.randn(layers_dims[l],layers_dims[l-1])*10
+        parameters['b' + str(l)] = np.zeros((layers_dims[l],1))
+
+    return parameters
+
+
+def initialize_parameters_he(layers_dims):
+
+    np.random.seed(3)
+    parameters = {}
+    L = len(layers_dims) - 1
+
+    for l in range(1, L + 1):
+        parameters['W' + str(l)] = np.random.randn(layers_dims[l],layers_dims[l-1])*np.sqrt(2./layers_dims[l-1])
+        parameters['b' + str(l)] = np.zeros((layers_dims[l],1))
+
+    return parameters
+
+
+def model(X, Y, learning_rate = 0.01, num_iterations = 15000, print_cost = True, initialization = "he"):
+
+    grads = {}
+    costs = []
+    m = X.shape[1]
+    layers_dims = [X.shape[0], 10, 5, 1]
+
+    if initialization == "zeros":
+        parameters = initialize_parameters_zeros(layers_dims)
+    elif initialization == "random":
+        parameters = initialize_parameters_random(layers_dims)
+    elif initialization == "he":
+        parameters = initialize_parameters_he(layers_dims)
+
+    for i in range(0, num_iterations):
+
+        a3, cache = forward_propagation(X, parameters)
+
+        cost = compute_loss(a3, Y)
+
+        grads = backward_propagation(X, Y, cache)
+
+        parameters = update_parameters(parameters, grads, learning_rate)
+
+        if print_cost and i % 1000 == 0:
+            print("Cost after iteration {}: {}".format(i, cost))
+            costs.append(cost)
+
+    plt.plot(costs)
+    plt.ylabel('cost')
+    plt.xlabel('iterations (per hundreds)')
+    plt.title("Learning rate =" + str(learning_rate))
+    plt.show()
+
+    return parameters
+
+
+if __name__ == '__main__':
+    train_X, train_Y, test_X, test_Y = load_dataset()
+    parameters = model(train_X, train_Y, initialization="he") # 选择initialization来切换初始化方法
+    print("On the train set:")
+    predictions_train = predict(train_X, train_Y, parameters)
+    print("On the test set:")
+    predictions_test = predict(test_X, test_Y, parameters)
